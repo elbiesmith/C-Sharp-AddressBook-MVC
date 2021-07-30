b58bde39f84009bf61f3b8a76afaa2145ef57936
BelleCode <Belinda.tiberio@gmail.com>
2021-07-30 08:14:52 -0700

    Add Models aAnd update Database Added IImages, Services, and Interfaces

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using C_Sharp_AddressBook_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_Sharp_AddressBook_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}

c0862a521fea63685a17af8ee5cea8e500339875
BelleCode <Belinda.tiberio@gmail.com>
2021-07-30 08:53:40 -0700

    Update with BasicImageService

---
+++ Services/BasicImageService.cs
@@ -0,0 +1,30 @@
+﻿using C_Sharp_AddressBook_MVC.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_Sharp_AddressBook_MVC.Services
+{
+    public class BasicImageService : IImageService
+    {
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            if (fileData is null) return string.Empty;
+
+            string imageBase64Data = Convert.ToBase64String(fileData);
+            return $"data:{extension}; Base64FormattingOptions,{ imageBase64Data}";
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            using MemoryStream memoryStream = new();
+            await file.CopyToAsync(memoryStream);
+            DivideByZeroException[] byteFile = memoryStream.ToArray();
+
+            return byteFile;
+        }
+    }
+}

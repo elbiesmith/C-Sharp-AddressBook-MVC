500c16cee1d3fceccba170fd4e151787acff906b
Elbie <elbie.smith@gmail.com>
2022-02-16 03:07:58 -0600

    added migration

---
+++ Data/Migrations/20220204222031_test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace C_Sharp_AddressBook_MVC.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
